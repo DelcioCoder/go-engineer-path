0db864abe8f85023ccb31e48494e97a54a3bca79
DelcioCoder <armandodelcio40@gmail.com>
2026-03-08 18:06:06 +0100

    tipos primitivos

--- mes-1-fundamentos/semana-1/pratica/03-tipos-primitivos/main.go
+++ mes-1-fundamentos/semana-1/pratica/03-tipos-primitivos/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
-func main(){
-	// Tipos primitivos
+func main() {
+	//Tipos primitivos
+	var itens int = 10
+	var preco float64 = 5.50
+
+	total := float64(itens) * preco
+	fmt.Println(total)
 }
