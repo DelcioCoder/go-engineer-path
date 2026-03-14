694da48f86629dd7ec06e4aa839cbd872a7e3865
DelcioCoder <armandodelcio40@gmail.com>
2026-03-14 18:37:34 +0100

    feat: implementa calculadora CLI (projeto final da semana 1)

--- mes-1-fundamentos/semana-1/pratica/main.go
+++ mes-1-fundamentos/semana-1/pratica/main.go
@@ -4,3 +4,63 @@
 package main
 
 // TODO: Implementar a calculadora aqui
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func soma(a, b float64) float64 {
+	return a + b
+}
+
+func multiplicacao(a, b float64) float64 {
+	return a * b
+}
+
+func subtracao(a, b float64) float64 {
+	return a - b
+}
+
+func divisao(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("divisão por zero não permitida!")
+	}
+
+	return a / b, nil
+}
+
+func main() {
+	fmt.Println("Calculadora CLI")
+	num1, err := strconv.ParseFloat(os.Args[1], 64)
+	if err != nil {
+		fmt.Println("Erro:", err)
+	}
+	operacao := os.Args[2]
+	num2, err := strconv.ParseFloat(os.Args[3], 64)
+	if err != nil {
+		fmt.Println("Erro:", err)
+	}
+
+	switch operacao {
+	case "+":
+		fmt.Println(soma(num1, num2))
+
+	case "*":
+		fmt.Println(multiplicacao(num1, num2))
+
+	case "-":
+		fmt.Println(subtracao(num1, num2))
+
+	case "/":
+		resultado, err := divisao(num1, num2)
+		if err != nil {
+			fmt.Println("Erro:", err)
+		}
+		fmt.Println(resultado)
+
+	default:
+		fmt.Println("Operação Inválida!!")
+	}
+}
